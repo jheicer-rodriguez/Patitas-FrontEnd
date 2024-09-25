1b3d800b5b9d833276be88e7c8af32709ddbfb9b
Jheicer <rodriguezolivaresjheicer@gmail.com>
2024-09-24 22:13:32 -0500

    Consumo api buenas practicas

--- src/main/java/pe/edu/cibertec/Patitas_FrontEnd/Config/RestTemplateConfig.java
+++ src/main/java/pe/edu/cibertec/Patitas_FrontEnd/Config/RestTemplateConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
         public RestTemplate restTemplate(RestTemplateBuilder builder) {
-            return builder.build();
+            return builder.setReadTimeout(Duration.ofSeconds(30)).build();
         }
 }
